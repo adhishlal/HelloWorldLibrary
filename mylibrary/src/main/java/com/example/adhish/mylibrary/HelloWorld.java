634398d238610f82fd77a722d813e71ca0669d01
adhishlal <adhish@adhishlal.com>
2017-04-19 10:28:23 +0530

    initial commit

---
+++ mylibrary/src/main/java/com/example/adhish/mylibrary/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.adhish.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by appiness on 19/4/17.
+ */
+
+public class HelloWorld {
+
+    public void logMessage(String message)
+    {
+        Log.d("MyLibrary",message);
+    }
+
+}
